3ac68a2e0c416879f882cc1913ec1034c8bd655c
harshthakur1509 <harshthakur1592@gmail.com>
2025-02-16 07:16:26 +0530

    removed unnecessary code

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -4,14 +4,6 @@ import (
 	"file-conv/internal/routes"
 )
 
-func init() {
-	// initializers.LoadEnv()
-	// initializers.ConnectDB()
-
-	// Add code here
-
-}
-
 func main() {
 	server := routes.NewApiServer(":3000")
 	server.Run()
--- server/internal/middleware/middleware.go
+++ server/internal/middleware/middleware.go
@@ -1,13 +1,10 @@
 package middleware
 
 import (
-	"context"
 	"log"
 	"net/http"
 	"runtime/debug"
 	"time"
-
-	"github.com/markbates/goth/gothic"
 )
 
 type responseWriterWrapper struct {
@@ -65,30 +62,6 @@ func RecoveryMiddleware(next http.Handler) http.HandlerFunc {
 		}()
 
 		next.ServeHTTP(w, r)
-	}
-}
-
-func AuthMiddleware(next http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Retrieve user ID from the session
-		userID, err := gothic.GetFromSession("user_id", r)
-		if err != nil || userID == "" {
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
-			return
-		}
-
-		// Refresh the session's lifetime
-		session, _ := gothic.Store.Get(r, gothic.SessionName)
-		session.Options.MaxAge += 86400 // Extend by 1 day (adjust as needed)
-		err = session.Save(r, w)
-		if err != nil {
-			http.Error(w, "Failed to refresh session", http.StatusInternalServerError)
-			return
-		}
-
-		// Attach user ID to the request context
-		ctx := context.WithValue(r.Context(), "userID", userID)
-		next.ServeHTTP(w, r.WithContext(ctx))
 	}
 }
 
